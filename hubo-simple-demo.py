8f8d53ecd1094b6e358b5281c115f92c8eebd47e
Daniel M. Lofaro <dan@danlofaro.com>
2013-08-15 19:27:32 -0700

    removed flush

--- hubo-simple-demo.py
+++ hubo-simple-demo.py
@@ -38,8 +38,8 @@ from ctypes import *
 # Open Hubo-Ach feed-forward and feed-back (reference and state) channels
 s = ach.Channel(ha.HUBO_CHAN_STATE_NAME)
 r = ach.Channel(ha.HUBO_CHAN_REF_NAME)
-s.flush()
-r.flush()
+#s.flush()
+#r.flush()
 
 # feed-forward will now be refered to as "state"
 state = ha.HUBO_STATE()
